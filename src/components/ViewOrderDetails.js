a6481e62295b264e44c9e6fa9f3fcef78bdb6e11
Esneider <esneider.m97@gmail.com>
2021-05-26 15:45:07 -0500

    order details warnings solved

--- src/components/ViewOrderDetails.js
+++ src/components/ViewOrderDetails.js
@@ -38,66 +38,84 @@ const ViewOrderDetails = props => {
 
   return (
     <div>
-      <ListGroup>
-        <h1>Order Details</h1>
-        <ListGroupItem>
-          Order information
-          <ListGroupItem>
-            external order number: {externalOrderNumber}
-          </ListGroupItem>
-          <ListGroupItem> buyer full name: {name} </ListGroupItem>
-          <ListGroupItem> buyer phone number: {phone} </ListGroupItem>
-          <ListGroupItem> buyer email: {email} </ListGroupItem>
-        </ListGroupItem>
-        <ListGroupItem>
-          Shipping info:
-          <ListGroupItem> shipping address: {address}</ListGroupItem>
-          <ListGroupItem> shipping city: {city}</ListGroupItem>
-          <ListGroupItem> shipping region: {region} </ListGroupItem>
-          <ListGroupItem> shipping country: {country} </ListGroupItem>
-        </ListGroupItem>
-        <ListGroupItem>
-          Promise dates
-          <ListGroupItem> pack_promise_min: {pack_promise_min} </ListGroupItem>
-          <ListGroupItem> pack_promise_max: {pack_promise_max} </ListGroupItem>
-          <ListGroupItem> ship_promise_min: {ship_promise_min} </ListGroupItem>
-          <ListGroupItem> ship_promise_min: {ship_promise_min} </ListGroupItem>
-          <ListGroupItem>
-            {" "}
-            delivery_promise_min: {delivery_promise_min}{" "}
-          </ListGroupItem>
-          <ListGroupItem>
-            {" "}
-            delivery_promise_max: {delivery_promise_max}{" "}
-          </ListGroupItem>
-          <ListGroupItem>
-            {" "}
-            ready_pickup_promise_min: {ready_pickup_promise_min}{" "}
-          </ListGroupItem>
-          <ListGroupItem>
-            {" "}
-            ready_pickup_promise_max: {ready_pickup_promise_max}{" "}
-          </ListGroupItem>
-        </ListGroupItem>
-        <ListGroupItem>
-          Line items (list of items)
-          <ListGroupItem>
-            {" "}
-            product name: {itemsData ? itemsData.itemsProductName : ""}{" "}
-          </ListGroupItem>
-          <ListGroupItem>
-            {" "}
-            product qty: {itemsData ? itemsData.itemsProductQty : ""}{" "}
-          </ListGroupItem>
-          <ListGroupItem>
-            {" "}
-            product weight: {itemsData ? itemsData.itemsProductWeight : ""}{" "}
-          </ListGroupItem>
-        </ListGroupItem>
-      </ListGroup>
-      <Button type="submit" onClick={() => history.push("/orders-list")}>
-        Go Back
-      </Button>
+      {orderDetails ? (
+        <div>
+          <ListGroup>
+            <h1>Order Details</h1>
+            <ListGroupItem>
+              Order information
+              <ListGroupItem>
+                external order number: {externalOrderNumber}
+              </ListGroupItem>
+              <ListGroupItem> buyer full name: {name} </ListGroupItem>
+              <ListGroupItem> buyer phone number: {phone} </ListGroupItem>
+              <ListGroupItem> buyer email: {email} </ListGroupItem>
+            </ListGroupItem>
+            <ListGroupItem>
+              Shipping info:
+              <ListGroupItem> shipping address: {address}</ListGroupItem>
+              <ListGroupItem> shipping city: {city}</ListGroupItem>
+              <ListGroupItem> shipping region: {region} </ListGroupItem>
+              <ListGroupItem> shipping country: {country} </ListGroupItem>
+            </ListGroupItem>
+            <ListGroupItem>
+              Promise dates
+              <ListGroupItem>
+                {" "}
+                pack_promise_min: {pack_promise_min}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                pack_promise_max: {pack_promise_max}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                ship_promise_min: {ship_promise_min}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                ship_promise_min: {ship_promise_min}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                delivery_promise_min: {delivery_promise_min}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                delivery_promise_max: {delivery_promise_max}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                ready_pickup_promise_min: {ready_pickup_promise_min}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                ready_pickup_promise_max: {ready_pickup_promise_max}{" "}
+              </ListGroupItem>
+            </ListGroupItem>
+            <ListGroupItem>
+              Line items (list of items)
+              <ListGroupItem>
+                {" "}
+                product name: {itemsData.itemsProductName}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                product qty: {itemsData.itemsProductQty}{" "}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                product weight: {itemsData.itemsProductWeight}{" "}
+              </ListGroupItem>
+            </ListGroupItem>
+          </ListGroup>
+          <Button type="submit" onClick={() => history.push("/orders-list")}>
+            Go Back
+          </Button>
+        </div>
+      ) : (
+        <div>The order was not found</div>
+      )}
     </div>
   );
 };
